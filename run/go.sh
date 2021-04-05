c5ea4cba87872a18366aebaff589da0e69b8a46e
Alan Layng <alan@readersludlow.com>
2021-04-05 22:01:28 +0100

    added manual shares

--- run/go.sh
+++ run/go.sh
@@ -4,5 +4,9 @@ dist/pixprices \
     --index ftse-aim-all-share \
     --sector alternative-investment-instruments \
     --stock DGI9 \
+    --stock CGI \
+    --stock LOK \
+    --stock AUG \
+    --stock TGP \
     --purge 7d \
     --publish
