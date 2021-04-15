228d796b832f4a101695b72b5b64a010ac88c2e6
Alan Layng <alan@readersludlow.com>
2021-04-15 14:50:12 +0100

    removing unused code

--- src/util.js
+++ src/util.js
@@ -4,10 +4,6 @@ export function delay (ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-export function toArr (x) {
-  return !x ? [] : Array.isArray(x) ? x : [x]
-}
-
 export function wrap (fn) {
   return (...args) =>
     Promise.resolve(fn(...args)).catch(err => {
