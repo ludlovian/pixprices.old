a1db98d357a8529b6853bca5dee3bd83b95624ff
Alan Layng <alan@readersludlow.com>
2023-04-25 10:37:34 +0100

    more tidying

--- run/go.sh
+++
@@ -1,9 +0,0 @@
-dist/pixprices \
-    update \
-    --import-portfolio \
-    --import-trades \
-    --import-stocks \
-    --fetch-prices \
-    --export-positions \
-    --export-trades \
-    --export-stocks
